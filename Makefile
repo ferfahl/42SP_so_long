140d3e55f1a5209954c139ef682188bdcee684cd
ferfahl <fe.alves.fahl@gmail.com>
2022-10-09 12:08:26 -0300

    mlx opens a window & libft works

---
+++ Makefile
@@ -0,0 +1,49 @@
+#####################INPUTS#####################
+
+#scr functions
+SRC_FILES =
+
+
+#static library's name
+NAME =	so_long.a
+
+#directories
+LIBFT_PATH =	./libs/libft
+LIBFT =			$(LIBFT_PATH)/libft.a
+MLX_PATH =		./libs/minilibx-linux
+MLX	 = $(MLX_PATH)/libmlx.a
+
+#header to libft.h
+INCLUDE = -I ./ -I $(LIBFT_PATH) -I $(MLX_PATH)
+
+#compiling
+CC =		gcc
+FLAGS =	-Wall -Werror -Wextra -g -O3 #-fsanitize=leak
+MLXFLAGS =	-lm -lXext -lX11
+
+# clean
+RM =		-rm -f
+RM_DIR =	rm -rf
+
+#tranform into .o
+OBJ = $(SRC_FILES:%.c=$(OBJPATH)/%.o)
+
+#####################RULES#####################
+
+#tester
+main:
+		cc $(FLAGS) main.c $(LIBFT) $(MLX) $(INCLUDE) $(MLXFLAGS) 
+
+#remove objects
+clean:
+		$(RM) $(OBJ)
+
+#remove all
+fclean: clean
+		$(RM) $(NAME)
+
+#clear all
+re: fclean all
+
+#avoids double inclusion
+.PHONY: all clean fclean re
---
+++ header/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feralves < feralves@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/09 15:26:55 by feralves          #+#    #+#             */
+/*   Updated: 2022/10/09 15:26:56 by feralves         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+//Libraries
+# include "../libs/libft/libft.h"
+# include "../libs/minilibx-linux/mlx.h"
+# include "../libs/minilibx-linux/mlx_int.h"
+
+//Functions
+
+#endif
