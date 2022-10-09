2fe066fcb63ea8e31ecde84ff73750f271c13d2b
ferfahl <fe.alves.fahl@gmail.com>
2022-10-09 14:27:04 -0300

    quick update

--- Makefile
+++ Makefile
@@ -12,6 +12,7 @@ LIBFT_PATH =	./libs/libft
 LIBFT =			$(LIBFT_PATH)/libft.a
 MLX_PATH =		./libs/minilibx-linux
 MLX	 = $(MLX_PATH)/libmlx.a
+OBJPATH = temps
 
 #header to libft.h
 INCLUDE = -I ./ -I $(LIBFT_PATH) -I $(MLX_PATH)
@@ -30,8 +31,16 @@ OBJ = $(SRC_FILES:%.c=$(OBJPATH)/%.o)
 
 #####################RULES#####################
 
+#make
+all: $(OBJPATH) $(NAME)
+
+#make folder for temps
+$(OBJPATH):
+		@mkdir -p $(OBJPATH)
+
 #tester
 main:
+		make -C ./libs/libft
 		cc $(FLAGS) main.c $(LIBFT) $(MLX) $(INCLUDE) $(MLXFLAGS) 
 
 #remove objects
@@ -40,10 +49,15 @@ clean:
 
 #remove all
 fclean: clean
-		$(RM) $(NAME)
+		$(RM) $(NAME) $(RM_DIR) $(OBJPATH)
 
 #clear all
 re: fclean all
 
 #avoids double inclusion
 .PHONY: all clean fclean re
+
+git:
+		git add .
+		git commit -m "quick update"
+		git push
--- main.c
+++ main.c
@@ -11,7 +11,10 @@
 /* ************************************************************************** */
 
 #include "./libs/minilibx-linux/mlx.h"
-
+#include "./libs/libft/libft.h"
+#include <stdlib.h>
+#include <X11/keysym.h>
+/* 
 int main()
 {
 	void	*mlx_ptr;
@@ -20,5 +23,45 @@ int main()
 	(void)win_ptr;
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0x000000);
 	mlx_loop(mlx_ptr);
+
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
+} */
+
+typedef struct	s_vars 
+{
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	esc_hook(int keycode, t_vars *vars)
+{
+	if (keycode == 65307)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit (0);
+	}
+	ft_printf("%d\n", keycode);
+	return (0);
+}
+
+int click_hook(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	exit (0);
+	return(0);
+}
+
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 500, 500, "42SP");
+	mlx_hook(vars.win, 2, 1L<<0, esc_hook, &vars);
+	mlx_hook(vars.win, 17, 1L<<2, click_hook, &vars);
+	mlx_loop(vars.mlx);
 }
