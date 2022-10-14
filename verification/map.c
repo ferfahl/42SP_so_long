b8985acedc01c1925f6bb46d49ab1de2c7078075
feralves <feralves@student.42sp.org.br>
2022-10-15 01:47:35 +0200

    organizing

---
+++ verification/map.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feralves < feralves@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/09 15:25:37 by feralves          #+#    #+#             */
+/*   Updated: 2022/10/15 01:44:28 by feralves         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/so_long.h"
+
+//read a file
+    //check file to turn into map (.ber)
+
+    //maps verifications
+    /*"if any misconfiguration of any kind is encountered in the file, the
+	program must exit in a clean way, and return "Error\n" followed by an
+    explicit error message of your choice."
+        //empty map
+        //not expected characters
+        //there can only be 1 player
+            //if theres more?
+        //not retangular
+        //no collectible
+        //at least 1 exit
+        //valid extension
+        //valid dimension
+        //surrounded by walls
+        //"valid path"
+
+// generate map archive
+    //verify all positions
+    //Verificar os botões (- e v) para não permitir que o jogo feche
+
+
+// BONUS
+    //Make the player lose when they touch an enemy patrol
+    //Add some sprite animation.
+    //Display the movement count directly on screen instead of writing it in the shell.
